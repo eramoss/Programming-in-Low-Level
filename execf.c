b8c31ce8abdc42abf0cb40f885e628f5684e100c
eramoss <edulramos@outlook.com>
2023-04-03 18:59:16 -0300

    add comment

--- execf.c
+++ execf.c
@@ -1,7 +1,13 @@
 #include <stdlib.h>
 
 int main(){
+  /*
+  just test for executing commands when adding in
+  variable PATH.
 
+  of course, this is just for testing
+  scripts in bash is more easy and practical
+  */
   system("sudo apt update");
 
   return 0;
