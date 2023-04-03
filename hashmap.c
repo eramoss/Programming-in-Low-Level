53b3d3474fad091000ccba53f7c86913d0a2b11c
eramoss <edulramos@outlook.com>
2023-04-03 19:32:49 -0300

    remove useless prefix name of struct Node

--- hashmap.c
+++ hashmap.c
@@ -26,7 +26,7 @@ int hash(char key[]){ // function hash returns a nem integer as a fingerprint of
 }
 
 
-typedef struct Node {
+typedef struct {
   char key[max_key_length];
   char value[value_length];
   struct Node * next;
