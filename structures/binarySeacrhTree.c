d9521e24fc266b81892e2951b1115eda642ad15d
eramoss <edulramos@outlook.com>
2023-04-04 12:15:44 -0300

    add comments

--- structures/binarySeacrhTree.c
+++ structures/binarySeacrhTree.c
@@ -8,7 +8,7 @@ typedef struct Node{
 } Node;
 
 Node * createNode(Node * root, int data){
-  if (root == NULL){
+  if (root == NULL){ // root is NULL, create a first instance of the node
     root = malloc(sizeof(Node));
     root->data = data;
     root->left = NULL;
@@ -16,30 +16,30 @@ Node * createNode(Node * root, int data){
     return root;
   } else {
     Node *current = root;
-    while(1){
+    while(1){ // infinite loop for walks through other nodes
       if (current->data > data){
-        if (current->left == NULL) {
+        if (current->left == NULL) { // last node > data
           Node * newNode = malloc(sizeof(Node));
           newNode->data = data;
           newNode->left = NULL;
           newNode->right = NULL;
-          current->left = newNode;
+          current->left = newNode; // set this new node to the tree
           return newNode;
           break;
         } else {
-          current = current->left;
+          current = current->left; // advance left child
         }
       } else if (current->data < data) {
-        if (current->right == NULL) {
+        if (current->right == NULL) { // last node < data
           Node * newNode = malloc(sizeof(Node));
           newNode->data = data;
           newNode->right = NULL;
           newNode->left = NULL;
-          current->right = newNode;
+          current->right = newNode; // add this new node to the tree
           return newNode;
           break;
         } else {
-          current = current->right;
+          current = current->right; // advance right child
         }
       } else {
         printf("ERROR: Invalid data, node cannot be inserted with same data of other nodes\n");
@@ -51,29 +51,29 @@ Node * createNode(Node * root, int data){
 }
 
 Node * Search(Node* root, int data){
-  if (root == NULL){
+  if (root == NULL){ // case tree is not initialized
     printf("Error: root cannot be NULL\n");
     return NULL;
   } else {
     Node *current = root;
-    while(1){
-      if (current->data == data){
+    while(1){ // walk all nodes
+      if (current->data == data){ // return current 
         return current;
-      } else if (current->data > data){
-        if (current->left == NULL) {
+      } else if (current->data > data){ 
+        if (current->left == NULL) { // verify if current node is last in tree
           printf("Error: Node not found\n");
           return NULL;
         }
-        current = current->left;
+        current = current->left; // advance left pointer
       } else if (current->data < data){
-          if (current->right == NULL) {
+          if (current->right == NULL) { // verify if current node is last in tree
           printf("Error: Node not found\n");
           return NULL;
         }
-        current = current->right;
+        current = current->right; // advance right pointer
       } else {
         printf("Error: Node not found\n");
-        return NULL;
+        return NULL; // return an error message if any cases are encountered
       }
     }
   }
