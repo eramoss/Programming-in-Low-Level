a7880b0b069d81b2e16bd45f3d2f8d63d497a052
eramoss <edulramos@outlook.com>
2023-04-17 10:26:22 -0300

    add error function and terminate expression with "\n"

--- calculator/main.cpp
+++ calculator/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include <string>
 #include <map>
+
 double number_value;
 std::string string_value;
 std::map<std::string, double> table;
 
-double prim(bool get);
+double prim(bool get); // pre assignment functions
+double term(bool get);
+double expr(bool get);
+
+int errors_amount;
+double error(const std::string& s){
+  errors_amount ++;
+  std::cerr << s << std::endl;
+  return 1;
+}
 
 typedef enum Token_value{
   NAME, NUMBER, END,
@@ -19,32 +29,70 @@ Token_value curr_token = IMPR;
 
 
 Token_value get_token(){
-  char ch = 0;
-  std::cin >> ch;
-
+  char ch;
+  do {
+    if (!std::cin.get(ch)) return curr_token = END;
+  } while (ch != '\n' && isspace(ch));
   switch (ch)
-  {
+  { 
     case 0:
       return curr_token = END;
-
-    case ';': case '*': case '/': case '+': case '-': case '(': case ')': case '=':
+    case ';': case '\n':
+      return curr_token = IMPR;
+    case '*': case '/': case '+': case '-': case '(': case ')': case '=':
       return curr_token = Token_value(ch);
 
     case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': case '.':
-      std::cin.putback(ch);
+      std::cin.putback(ch);  // case ch is a number, put the ch value on number value and return current token as a NUMBER.
       std::cin >> number_value;
       return curr_token = NUMBER;
 
-    default:
+    default: {
       if (isalpha(ch)) {
+        string_value = ch;
+        while( std::cin.get(ch) && isalnum(ch)) string_value.push_back(ch); 
         std::cin.putback(ch);
-        std::cin >> string_value;
+        if(string_value == "clear") system("clear");
         return curr_token = NAME;
       }
-      throw std::runtime_error("invalid character token");
+      error("invalid character token");
       return curr_token = IMPR;
+    }
   }
 }
+
+double prim(bool get){
+  if (get) get_token();
+
+  switch (curr_token){
+    case NUMBER:{
+      double v = number_value;
+      get_token();
+      return v;
+    }
+      
+    case NAME:{
+      double & v = table[string_value];
+      if (get_token() == ATT) v = expr(true);
+      return v;
+    }
+      
+    case MINUS: {
+      return -prim(true);
+    }
+      
+    case BRACK_L:{
+      double e = expr(true);
+      if (curr_token != BRACK_R) error("expected bracket \")\"");
+      get_token();
+      return e;
+    }
+      
+    default:
+      return error("expected primary expression");
+  }
+}
+
 
 
 double term(bool get){
@@ -62,7 +110,7 @@ double term(bool get){
           esq /= d;
           break;
         }
-        throw std::runtime_error("invalid division: The divisor is NULL");
+        return error("invalid division: The divisor is NULL");
       }
         
       default: 
@@ -70,7 +118,6 @@ double term(bool get){
     }
   }
 }
-
 
 
 double expr(bool get){
@@ -96,48 +143,16 @@ double expr(bool get){
 }
 
 
-double prim(bool get){
-  if (get) get_token();
-
-  switch (curr_token){
-    case NUMBER:
-    {
-      double v = number_value;
-      get_token();
-      return v;
-
-    }
-      
-    case NAME:{
-      double & v = table[string_value];
-      if (get_token() == ATT) v = expr(true);
-      return v;
-    }
-      
-    case MINUS: {
-      return -prim(true);
-    }
-      
-    case BRACK_L:{
-      double e = expr(true);
-      if (curr_token != BRACK_R) throw std::runtime_error("expected bracket \")\"");
-      get_token();
-      return e;
-    }
-      
-    default:
-      throw std::runtime_error("expected primary expression");
-  }
-}
-
-
-
-
-
 
 int main(int argc, char const *argv[])
 {
-  std::cout << "Put the arithmetic expression:\n";
-  std::cout << expr(true) << std::endl;
-  return 0;
+  table["pi"] = 3.1315926535897932385;
+  table["e"] = 2.7182818284590452354;
+  while(std::cin) {
+    get_token();
+    if(curr_token == END) break;
+    if(curr_token == IMPR) continue;
+    std::cout << expr(false) << "\n";
+  }
+  return errors_amount;
 }
