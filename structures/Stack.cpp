b15dda482a4b9a8da09c2e2a4aae0becf21764b8
eramoss <edulramos@outlook.com>
2023-06-19 08:49:42 -0300

    change struct to class

--- structures/Stack.cpp
+++ structures/Stack.cpp
@@ -1,30 +1,32 @@
-/* this code is for the exercise 1 at chapter 8.5 in 
+/* this code is for the exercise 1 at chapter 8.5 in
 "c++ programming language - by Bjarne Stroustrup" */
 #include <iostream>
-typedef struct Node {
-  Node(int d){
+class Node {
+  public:
+  Node(int d) {
     data = d;
     prev = NULL;
     next = NULL;
   }
   int data;
-  Node *next;
-  Node *prev;
-} Node;
+  Node* next;
+  Node* prev;
+};
 
- Node * current = NULL;
- Node * previous = NULL;
- Node * root = NULL;
- int nodes;
+Node* current = NULL;
+Node* previous = NULL;
+Node* root = NULL;
+int nodes;
 
-Node * createNode(int data){
-  Node * newNode = new Node(data);
-  if(!current){
+Node* createNode(int data) {
+  Node* newNode = new Node(data);
+  if (!current) {
     current = newNode;
     root = newNode;
-  } else {
+  }
+  else {
     previous = current;
-    current = newNode;  
+    current = newNode;
     newNode->prev = previous;
     newNode->prev->next = current;
   }
@@ -32,7 +34,7 @@ Node * createNode(int data){
   return newNode;
 }
 
-void Pop(){
+void Pop() {
   std::cout << "last node: " << current->data << " is deleted" << std::endl;
   delete current;
   previous->next = NULL;
@@ -42,37 +44,37 @@ void Pop(){
 
 
 static int length;
-void Sort(){
+void Sort() {
 
-  if(root->next){
+  if (root->next) {
 
-    for(Node *curr = root; curr; curr = curr->next){
-      for(Node *next = curr->next; next; next = next->next){
-        if(curr->data > next->data){
+    for (Node* curr = root; curr; curr = curr->next) {
+      for (Node* next = curr->next; next; next = next->next) {
+        if (curr->data > next->data) {
           curr->data = curr->data + next->data; /*GAMBIARRA*/
           next->data = curr->data - next->data;
           curr->data = curr->data - next->data;
         }
       }
     }
-    for(Node *curr = root; curr; curr = curr->next){
-    std::cout << curr->data << std::endl;
+    for (Node* curr = root; curr; curr = curr->next) {
+      std::cout << curr->data << std::endl;
     }
 
-  return;
-  } else return;
+    return;
+  }
+  else return;
 }
 
-int main(int argc, char const *argv[])
-{
-  Node * test = createNode(7000);
-  Node * test1 = createNode(60);
-  Node * test2 = createNode(700);
-  Node * test3 = createNode(50);
-  Node * test4 = createNode(61);
+int main(int argc, char const* argv[]) {
+  Node* test = createNode(7000);
+  Node* test1 = createNode(60);
+  Node* test2 = createNode(700);
+  Node* test3 = createNode(50);
+  Node* test4 = createNode(61);
 
   Sort();
-  
+
 
   return 0;
 }
