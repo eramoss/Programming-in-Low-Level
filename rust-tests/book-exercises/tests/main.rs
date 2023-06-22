c984e0eccdfb1fe07d0791b1809a936cf58ee23e
eramoss <edulramos@outlook.com>
2023-06-22 15:24:56 -0300

    add random to test of nth element fibonacci

--- rust-tests/book-exercises/tests/main.rs
+++ rust-tests/book-exercises/tests/main.rs
@@ -2,6 +2,9 @@ use book_exercises::exercises::nth_fibonacci_num::nth_element;
 
 #[test]
 fn nth_element_test() {
-    let x = nth_element(10);
-    assert_eq!(x, 55);
+    let semi_fibonacci_seq = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55];
+    let rand_n = (rand::random::<usize>() % 9) + 1;
+    let y = semi_fibonacci_seq[rand_n - 1];
+    let x = nth_element(rand_n);
+    assert_eq!(x, y);
 }
